c539b282afed22ccd0f1ab5de9bad8840753d7d5
safy97 <mostafasafy1997@gmail.com>
2019-04-28 11:34:11 +0000

    add article and category association

---
+++ db/migrate/20190428110437_create_articles_catgories.rb
@@ -0,0 +1,8 @@
+class CreateArticlesCatgories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :article_categories do |t|
+      t.integer :article_id
+      t.integer :category_id
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_27_070144) do
+ActiveRecord::Schema.define(version: 2019_04_28_110437) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
